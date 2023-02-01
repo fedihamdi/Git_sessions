b3a0166dc9a263955fcb2c1f35f14d13ac26dd21
Fedi Hamdi <fedihamdi.jr@gmail.com>
2023-02-01 23:42:09 +0100

    Update find_out.sh

--- Shell_Sessions/find_out.sh
+++ Shell_Sessions/find_out.sh
@@ -4,13 +4,20 @@ exec=0
 no_exec=0
 var rep=0
 for i in `ls $1`;
- do if [ -x $i ]; 
-then  
-let "exec = $exec+1";
+ do 
+ if [ -x $i ]; 
+ then  
+ let "exec = $exec+1";
  echo $i >> ~/list-exec.txt;
-else 
+ elif [ -d $1/$f ]
+		let "rep=$rep+1"
+		echo $f >> list_rep.txt
+	else 
+		let "no_exec=$no_exec+1"
+		echo $f >> list_no_exec.txt
+ else 
  echo "for now there is $exec files"
  let "no_exec = $no_exec+1";
  echo "for now there is $no_exec files non-exec"
-fi; 
+ fi; 
 done
